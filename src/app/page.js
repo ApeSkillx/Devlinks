2da9262d52481a8f9751e327a034dcbe1325eb66
ApeSkillx <mrkant19@gmail.com>
2024-10-25 13:10:54 +0530

    Renamed folder from customize-links to userdashboard

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   // Customize links is the home page.
-  redirect("/customize-links");
+  redirect("/userdashboard");
 }
